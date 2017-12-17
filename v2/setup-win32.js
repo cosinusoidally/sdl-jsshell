229c6129e1ea7834fec475bdec4385f8b6f3437b
Liam Wilson <cosinusoidally@gmail.com>
2017-12-17 10:25:52 +0000

    win32 instructions

--- v2/setup-win32.js
+++ v2/setup-win32.js
@@ -1,4 +1,34 @@
 /*
+
+Setting up on win32.
+--------------------
+
+Get win32 jsshell binaries from:
+
+https://github.com/cosinusoidally/jsshell-binaries-mirror
+
+Check the signature to confirm that they are authenic versions from Mozilla (or
+if you prefer, get them straight from Mozilla).
+
+Get the win32 version of SDL 1.2 from here:
+
+https://www.libsdl.org/download-1.2.php
+
+direct link is:
+
+https://www.libsdl.org/release/SDL-1.2.15-win32.zip
+
+Extract both jsshell and SDL. Put the SDL.dll file in to the same directory as
+js.exe
+
+You should then be able to run the test.js file using:
+
+js.exe test.js
+
+*/
+
+
+/*
 This is a (inefficient) polyfill implementation of SDL_AddTimer and
 SDL_WaitEvent.
 
