3bac75854ae62ef16b8b574cc7ae09c4b8b66a11
Liam Wilson <cosinusoidally@gmail.com>
2018-01-05 15:56:26 +0000

    [better-timer] get function address of libcb.cb from JS

--- wip/better-timer/sdl.js
+++ wip/better-timer/sdl.js
@@ -415,6 +415,10 @@ libcb.init=libcb.lib.declare("init",ctypes.default_abi, ctypes.void_t);
 libcb.getmut=libcb.lib.declare("getmut",ctypes.default_abi, ctypes.voidptr_t);
 libcb.getcond=libcb.lib.declare("getcond",ctypes.default_abi, ctypes.voidptr_t);
 
+// note that this is the wrong signature for libcb.cb, I only ever use its
+// address and never call it from JS so it doesn't matter.
+libcb.cb=libcb.lib.declare("cb",ctypes.default_abi, ctypes.void_t);
+
 libcb.cond=libcb.getcond();
 libcb.mut=libcb.getmut();
 
