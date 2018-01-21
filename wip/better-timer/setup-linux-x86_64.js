dbff366c30c4c2b8f528ef750d7e3867d181c984
Liam Wilson <cosinusoidally@gmail.com>
2018-01-21 21:14:23 +0000

    [better-timer] update docs in setup-linux-x86_64.js

--- wip/better-timer/setup-linux-x86_64.js
+++ wip/better-timer/setup-linux-x86_64.js
@@ -1,3 +1,17 @@
+/*
+
+In this file we load and setup our timer callback function for linux-x86_64.
+
+First we need some machine code. We generate the machine code by compiling
+cb-snippet-64bit.c . This gives us cb-snippet-64bit.o . We then disasseble the
+machine code with objdump and use it to create at libcb.cb_bin array containing
+the machine code.
+
+See below for the current version of our machine code:
+
+*/
+
+
 libcb.cb_bin=[
 //$ objdump -d -S cb-snippet-64bit.o 
 
@@ -37,6 +51,9 @@ libcb.cb_bin=[
 //  1c:	c3                   	retq   
 	0xc3];
 
+// Now we need to do a bit of a dance to get the machine code into an
+// ArrayBuffer
+
 libcb.cb_raw=new ArrayBuffer(libcb.cb_bin.length);
 
 // convert libcb.cb_bin in to a Uint8Array view onto libcb.cb_raw
@@ -49,14 +66,33 @@ libcb.cb_raw=new ArrayBuffer(libcb.cb_bin.length);
   libcb.cb_bin=o;
 })();
 
+// At this point libcb.cb_raw is our ArrayBuffer and libcb.cb_bin is now
+// a Uint8Array view onto that ArrayBuffer.
 
-// First we must get the address of SDL_CondSignal and the condition variables
+/*
+
+
+First we must get the address of SDL_CondSignal and the condition variables
+
+*/
 
 sdl.address_SDL_CondSignal=ctypes.cast(sdl.SDL_CondSignal,ctypes.uint8_t.array(8));
 libcb.address_cond=ctypes.cast(libcb.cond,ctypes.uint8_t.array(8));
 
+/*
+Next we must patch up our machine code. The relevant part of the above machine code is:
 
-// patch in address of SDL_CondSignal
+((my_SDL_CondSignal)0xdeadbeefdeadbeef)(0x1234567890abcdef);
+   3:	48 b8 ef be ad de ef 	movabs $0xdeadbeefdeadbeef,%rax
+   a:	be ad de 
+   d:	48 bf ef cd ab 90 78 	movabs $0x1234567890abcdef,%rdi
+  14:	56 34 12 
+
+Now 0xdeadbeefdeadbeef needs to be replaced with the address of SDL_CondSignal.
+We can see that 0xdeadbeefdeadbeef is at offset 5
+
+With this offset we then patch in address of SDL_CondSignal
+*/
 
 (function(){
   for(var i=0;i<8;i++){
@@ -64,14 +100,25 @@ libcb.address_cond=ctypes.cast(libcb.cond,ctypes.uint8_t.array(8));
   }
 })();
 
-// patch in address off condition variable (libcb.address_cond)
+/*
 
+Now, the 0x1234567890abcdef above needs to be the address of our Condition Variable.
+As we can see, the offset of 0x1234567890abcdef is 0xf
 
+Patch in address off condition variable (libcb.address_cond):
+
+*/
 (function(){
   for(var i=0;i<8;i++){
     libcb.cb_bin[i+0xf]=libcb.address_cond[i];
   }
 })();
+
+/*
+
+Now we must prepare some executable memory for our machine code
+
+*/
 
 libcb.cb=libc.mmap(                       sdl.voidptr,
                                                4096,
@@ -80,7 +127,17 @@ libcb.cb=libc.mmap(                       sdl.voidptr,
                                                   0,
                                                   0);
 
+// Finally we copy our machine code in to our executable memory
 
-// Next we copy our machine code in to our sdl.cb buffer.
 libc.memcpy(libcb.cb,libcb.cb_raw,libcb.cb_bin.length);
 
+/*
+For security purposes we should now mprotect the memory to make it read only
+and executable, but for now we just leave it as executable and read/writable.
+
+Speficially we should initially map the memory as r/w. Copy across the machine
+code, and then mprotect the memory to make it execute only.
+
+It's not a major risk at the moment (since we are not running untrusted JS code),
+but if I add the ability to run untrusted code I would probably need to fix this.
+*/
